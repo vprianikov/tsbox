6496409ef24d5d25188303e097eebc48d9111b49
Vasilii Prianikov <88191516+vprianikov@users.noreply.github.com>
2026-07-31 00:21:36 +0300

    refactor: migrate project to typescript

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,14 +4,16 @@ import markdown from "@eslint/markdown";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ["**/*.{js}"],
+    files: ["**/*.{ts}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
   },
+  tseslint.configs.recommended,
   {
     files: ["**/*.json"],
     ignores: ["**/package-lock.json"],
